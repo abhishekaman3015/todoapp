66d1e888ebf94c1bed33f4bfff413d291ca9c07a
Abhishek Kumar Aman <abhishekaman3015@gmail.com>
2021-05-26 18:21:54 +0530

    Updated Login Component

--- src/components/ToDoApp/ToDoApp.jsx
+++ src/components/ToDoApp/ToDoApp.jsx
@@ -78,8 +78,10 @@ export class LoginComponent extends Component {
     render() {
         return (
             <div>
-                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
-                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
+                {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>*/}
+                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
+                {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>*/}
+                {this.state.showSuccessMessage && <div>Login Successful</div>}
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button onClick={this.loginClicked}>Login</button>
@@ -88,19 +90,19 @@ export class LoginComponent extends Component {
     }
 }
 
-function ShowInvalidCredentials(props){
-    if (props.hasLoginFailed){
-        return <div>Invalid Credentials</div>
-    }
-    return null
-}
+// function ShowInvalidCredentials(props){
+//     if (props.hasLoginFailed){
+//         return <div>Invalid Credentials</div>
+//     }
+//     return null
+// }
 
-function ShowLoginSuccessMessage(props){
-    if (props.showSuccessMessage){
-        return <div>Login Successful</div>
-    }
-    return null
-}
+// function ShowLoginSuccessMessage(props){
+//     if (props.showSuccessMessage){
+//         return <div>Login Successful</div>
+//     }
+//     return null
+// }
 
 
 export default ToDoApp
