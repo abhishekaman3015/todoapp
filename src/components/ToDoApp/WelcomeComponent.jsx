3f9923b4efa943f5dc5e5a2c86137888774623b2
Abhishek Kumar Aman <abhishekaman3015@gmail.com>
2021-06-08 12:07:56 +0530

    modified WelcomeComponent

--- src/components/ToDoApp/WelcomeComponent.jsx
+++ src/components/ToDoApp/WelcomeComponent.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 
 export class WelcomeComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.getWelcomeMessage = this.getWelcomeMessage.bind(this)
+    }
     render() {
         return (
             <>
@@ -9,8 +13,16 @@ export class WelcomeComponent extends Component {
                 <div className="container">
                 Welcome to ToDoApp {this.props.match.params.name}. you can manage your todos <Link to="/todos">Here</Link>
                 </div>
+
+                <div className="container">
+                    Click here to get the customized welcome Message.
+                    <button onClick={this.getWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
             </>
         )
+    }
+    getWelcomeMessage(){
+        console.log('button clicked')
     }
 }
 
